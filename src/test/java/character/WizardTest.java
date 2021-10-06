a84f341d1eeeaa4ff1953e689b60a85e2f678ef4
Derek Bracken <derekbracken@gmail.com>
2021-10-06 14:20:07 +0100

    Creature tests passed

--- src/test/java/character/WizardTest.java
+++ src/test/java/character/WizardTest.java
@@ -1,7 +1,11 @@
 package character;
 
 import character.mage.Wizard;
+import enums.Creatures;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class WizardTest {
 
@@ -10,5 +14,18 @@ public class WizardTest {
     @Before
     public void before(){
         wizard = new Wizard(100);
+    }
+
+    @Test
+    public void canAddCreature(){
+        wizard.addCreature(Creatures.DRAGON);
+        assertEquals(1, wizard.getCreatures().size());
+    }
+
+    @Test
+    public void canEquipCreature(){
+        wizard.addCreature(Creatures.DRAGON);
+        wizard.equipCreature(Creatures.DRAGON);
+        assertEquals(Creatures.DRAGON, wizard.getCurrentCreature());
     }
 }
